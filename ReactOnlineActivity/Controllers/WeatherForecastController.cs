02142e686cba02fac88e498c0ba32cdc0d8e8a10
Sanchezz17 <gekachan2000@gmail.com>
2020-05-04 14:23:01 +0500

    dotnet new react ...

--- ReactOnlineActivity/Controllers/WeatherForecastController.cs
+++ ReactOnlineActivity/Controllers/WeatherForecastController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace OnlineActivity.Controllers
+namespace ReactOnlineActivity.Controllers
 {
     [Authorize]
     [ApiController]
@@ -30,12 +30,12 @@ namespace OnlineActivity.Controllers
         {
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-                {
-                    Date = DateTime.Now.AddDays(index),
-                    TemperatureC = rng.Next(-20, 55),
-                    Summary = Summaries[rng.Next(Summaries.Length)]
-                })
-                .ToArray();
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
         }
     }
 }
--- ReactOnlineActivity/WeatherForecast.cs
+++ ReactOnlineActivity/WeatherForecast.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace OnlineActivity
+namespace ReactOnlineActivity
 {
     public class WeatherForecast
     {
@@ -8,7 +8,7 @@ namespace OnlineActivity
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
     }
