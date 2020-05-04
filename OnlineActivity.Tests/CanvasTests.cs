bf655a22fc0f66f672b367747d1bb2a97117c813
lgnv <43182296+lgnv@users.noreply.github.com>
2020-05-04 20:00:20 +0500

    Добавлены тесты для Game и Canvas

---
+++ OnlineActivity.Tests/CanvasTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using ReactOnlineActivity.Services;
+
+namespace OnlineActivity.Tests
+{
+    [TestFixture]
+    public class CanvasTests
+    {
+        private Canvas _canvas;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _canvas = new Canvas(20, 20);
+        }
+        
+        [TestCase(10, 10, TestName = "WhenSizesSameAndPositive")]
+        [TestCase(10, 15, TestName = "WhenSizeDifferentAndPositive")]
+        public void Constructor_DoesNotThrow(int x, int y)
+        {
+            Action action = () => new Canvas(x, y);
+            action.Should().NotThrow();
+        }
+
+        [TestCase(0, 3, TestName = "WhenOneCoordinatesIsZero")]
+        [TestCase(3, -1, TestName = "WhenOneCoordinatesIsNegative")]
+        public void Constructor_ShouldThrowArgumentException(int x, int y)
+        {
+            Action action = () => new Canvas(x, y);
+            action.Should().Throw<ArgumentException>(); 
+        }
+
+        [TestCase(0, 0, 1, 2, 3, 4, 5)]
+        public void PaintOverPixels_ShouldPaint(int x, params int[] coordinatesY)
+        {
+            var color = new RGB(255, 255, 255);
+            var pixels = coordinatesY
+                .Select(y => new Pixel(
+                    color, 
+                    new Point(x, y)
+                    ));
+            
+            
+            _canvas.PaintOverPixels(pixels);
+
+
+            foreach (var y in coordinatesY)
+                _canvas.Pixels[x, y].Should().Be(color);
+        }
+        
+        [TestCase(0, 0, 1, 2, 3, 4, 5)]
+        public void PaintOverPixels_ShouldNotPaintUnnecessary(int x, params int[] coordinatesY)
+        {
+            var pixels = coordinatesY
+                .Select(y => new Pixel(
+                    new RGB(255, 255, 255), 
+                    new Point(x, y)
+                ));
+            var previousCount = CountPainted(_canvas);
+
+
+            _canvas.PaintOverPixels(pixels);
+            
+            CountPainted(_canvas).Should().Be(previousCount + pixels.Count());
+        }
+
+        private IEnumerable<(int, int)> Range2D(int startX, int countX, int startY, int countY)
+        {
+            return Enumerable
+                .Range(startX, countX)
+                .Select(x => Enumerable.Range(startY, countY).Select(y => (x, y)))
+                .SelectMany(e => e);
+        }
+
+        private int CountPainted(Canvas canvas)
+        {
+            return Range2D(0, canvas.Size.Width, 0, canvas.Size.Height)
+                .Count(tuple =>
+            {
+                var (x, y) = tuple;
+                var rgb = canvas.Pixels[x, y];
+                return rgb.R == 255 && rgb.G == 255 && rgb.B == 255;
+            });
+        }
+    }
+}
---
+++ OnlineActivity.Tests/GameTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using ReactOnlineActivity.Services;
+
+namespace OnlineActivity.Tests
+{
+    [TestFixture]
+    public class GameTests
+    {
+        [TestCase("TIMURCHEK")]
+        public void Constructor_DoesNotThrow(string word)
+        {
+            Action action = () => new Game(word);
+            action.Should().NotThrow();
+        }
+        
+        [TestCase(null, TestName = "WhenWordIsNull")]
+        public void Constructor_ShouldThrowArgumentException(string word)
+        {
+            Action action = () => new Game(word);
+            action.Should().Throw<ArgumentException>();
+        }
+
+        [TestCase("1", "1", TestName = "WhenSame")]
+        [TestCase("1", "   1 ", TestName = "WhenThereIdent")]
+        public void CheckWord_ShouldReturnsTrue(string hiddenWord, string playerWord)
+        {
+            var game = new Game(hiddenWord);
+            game.CheckWord(playerWord).Should().BeTrue();
+        }
+        
+        [TestCase("1", "2", TestName = "WhenDifferent")]
+        public void CheckWord_ShouldReturnsFalse(string hiddenWord, string playerWord)
+        {
+            var game = new Game(hiddenWord);
+            game.CheckWord(playerWord).Should().BeFalse();
+        }
+    }
+}
