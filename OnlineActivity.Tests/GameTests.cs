d44a408aaf42133bb8867eb593e8c827c7eb4895
lgnv <43182296+lgnv@users.noreply.github.com>
2020-05-05 13:54:07 +0500

    рефакторинг тестов

--- OnlineActivity.Tests/GameTests.cs
+++ OnlineActivity.Tests/GameTests.cs
@@ -11,14 +11,14 @@ namespace OnlineActivity.Tests
         [TestCase("TIMURCHEK")]
         public void Constructor_DoesNotThrow(string word)
         {
-            Action action = () => new Game(word);
+            Action action = () => new Game(new []{ word }, new Player[0]);
             action.Should().NotThrow();
         }
         
         [TestCase(null, TestName = "WhenWordIsNull")]
         public void Constructor_ShouldThrowArgumentException(string word)
         {
-            Action action = () => new Game(word);
+            Action action = () => new Game(new []{ word }, new Player[0]);
             action.Should().Throw<ArgumentException>();
         }
 
@@ -26,14 +26,14 @@ namespace OnlineActivity.Tests
         [TestCase("1", "   1 ", TestName = "WhenThereIdent")]
         public void CheckWord_ShouldReturnsTrue(string hiddenWord, string playerWord)
         {
-            var game = new Game(hiddenWord);
+            var game = new Game(new []{ hiddenWord }, new Player[0]);
             game.CheckWord(playerWord).Should().BeTrue();
         }
         
         [TestCase("1", "2", TestName = "WhenDifferent")]
         public void CheckWord_ShouldReturnsFalse(string hiddenWord, string playerWord)
         {
-            var game = new Game(hiddenWord);
+            var game = new Game(new []{ hiddenWord }, new Player[0]);
             game.CheckWord(playerWord).Should().BeFalse();
         }
     }
