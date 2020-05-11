512176c2bd677266b5042e2030fddda604b2d4e1
lgnv <43182296+lgnv@users.noreply.github.com>
2020-05-11 13:53:31 +0500

    Добавил сущности настроек комнаты и тем

---
+++ Game/Domain/Theme.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Game.Domain
+{
+    public class Theme
+    {
+        public string Name { get; set; }
+        public List<string> Words { get; set; }
+    }
+}
