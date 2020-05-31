7fe2c3ac1405f409baef8ae6b8190c6eb4b8260c
Evgeny <42266699+Sanchezz17@users.noreply.github.com>
2020-05-31 13:56:21 +0500

    константы UserStatistics для фронта

---
+++ ReactOnlineActivity/ClientApp/src/components/Leaderboard/LeaderboardConstants.js
@@ -0,0 +1,6 @@
+﻿export const UserStatistics = {
+    TOTAL_SCORE: 'totalScore',
+    NUMBER_OF_GAMES_PLAYED: 'numberOfGamesPlayed',
+    NUMBER_OF_DRAWS: 'numberOfDraws',
+    WINS_COUNT: 'winsCount'
+};
