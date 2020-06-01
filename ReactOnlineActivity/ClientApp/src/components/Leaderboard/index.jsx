ac6c0f2d7b03cf493f19e460cbc26cd2ed80e3db
Evgeny <42266699+Sanchezz17@users.noreply.github.com>
2020-06-01 11:52:19 +0500

    Рефакторинг

--- ReactOnlineActivity/ClientApp/src/components/Leaderboard/index.jsx
+++ ReactOnlineActivity/ClientApp/src/components/Leaderboard/index.jsx
@@ -9,7 +9,7 @@ export default class Leaderboard extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            users: [],
+            bestUsers: [],
             loadingUsers: true
         };
     }
@@ -21,46 +21,53 @@ export default class Leaderboard extends Component {
     onChangeSelectedSort = (event) => {
         const { value } = event.target;
         this.fetchUsers(value);
-    }
+    };
 
-    fetchUsers = async (desiredStatistics, limit=DEFAULT_LIMIT) => {
-        const users = await authorizeFetch(`/api/users?desiredStatistics=${desiredStatistics}&limit=${limit}`);
+    fetchUsers = async (desiredStatistics, limit = DEFAULT_LIMIT) => {
+        const bestUsers = await authorizeFetch(`/api/users?desiredStatistics=${desiredStatistics}&limit=${limit}`);
         this.setState({
             loadingUsers: false,
-            users
+            bestUsers
         });
     };
 
+    renderUsers(users) {
+        return users.map(user =>
+            <div className={styles.user} key={user.name}>
+                <div>
+                    {user.photoUrl &&
+                    <img className={styles.user__image} src={user.photoUrl} alt={user.name}/>}
+                    <span className={`${styles.user__name} `}>{user.name}</span>
+                </div>
+                <p className={styles.user__score}>{user.statistics.totalScore}</p>
+                <p className={styles.user__gamesCount}>{user.statistics.numberOfGamesPlayed}</p>
+                <p className={styles.user__drawsCount}>{user.statistics.numberOfDraws}</p>
+                <p className={styles.user__winsCount}>{user.statistics.winsCount}</p>
+            </div>
+        );
+    }
+
     render() {
-        const { users, loadingUsers } = this.state;
+        const { bestUsers, loadingUsers } = this.state;
+
 
         return (
             <>
                 <h1>Список лидеров</h1>
-                
-                {loadingUsers ? <p>Загрузка игроков...</p>:
+
+                {loadingUsers ? <p>Загрузка игроков...</p> :
                     <>
                         <label htmlFor="leaders">Сортировать </label>
 
                         <select name="leaders" id="leaders" onChange={this.onChangeSelectedSort} on>
-                            <option value={ UserStatistics.TOTAL_SCORE } selected='selected'>по очкам</option>
-                            <option value={ UserStatistics.NUMBER_OF_GAMES_PLAYED }>по играм</option>
-                            <option value={ UserStatistics.NUMBER_OF_DRAWS }>по количеству рисований</option>
-                            <option value={ UserStatistics.WINS_COUNT }>по победам</option>
+                            <option value={UserStatistics.TOTAL_SCORE} selected='selected'>по очкам</option>
+                            <option value={UserStatistics.NUMBER_OF_GAMES_PLAYED}>по играм</option>
+                            <option value={UserStatistics.NUMBER_OF_DRAWS}>по количеству рисований</option>
+                            <option value={UserStatistics.WINS_COUNT}>по победам</option>
                         </select>
 
-                        <section className={styles.board}>{users.map(user =>
-                            <div className={styles.user} key={user.name}>
-                                <div>
-                                    {user.photoUrl && <img className={styles.user__image} src={user.photoUrl} alt={user.name}/>}
-                                    <span className={`${styles.user__name} `}>{user.name}</span>
-                                </div>
-                                <p className={styles.user__score}>{user.Statistics.TotalScore}</p>
-                                <p className={styles.user__gamesCount}>{user.Statistics.NumberOfGamesPlayed}</p>
-                                <p className={styles.user__drawsCount}>{user.Statistics.NumberOfDraws}</p>
-                                <p className={styles.user__winsCount}>{user.Statistics.WinsCount}</p>
-                            </div>
-                        )}
+                        <section className={styles.board}>
+                            {this.renderUsers(bestUsers)}
                         </section>
                     </>
                 }
--- ReactOnlineActivity/Models/UserStatistics.cs
+++ ReactOnlineActivity/Models/UserStatistics.cs
@@ -2,11 +2,10 @@
 {
     public class UserStatistics
     {
+        public int Id { get; set; }
         public int TotalScore { get; set; }
         public int NumberOfGamesPlayed { get; set; }
         public int NumberOfDraws { get; set; }
         public int WinsCount { get; set; }
-        
-        public int id { get; set; }
     }
 }
